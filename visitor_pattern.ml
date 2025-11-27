4c1d198699fe90a0d25cfd3da2444df8c5f1e59c
猗露 <69190413+illusory0x0@users.noreply.github.com>
2025-11-28 07:58:50 +0800

    save

--- visitor_pattern.ml
+++ visitor_pattern.ml
@@ -1,20 +1,68 @@
+(* module type Data = sig
+    type a 
+end *)
+module Data = struct
+  module type S = sig
+    type a
+  end
+end
+
+module Expr (S : Data.S) = struct
+  class virtual ['visitor] t =
+    object
+      method virtual accept : 'visitor -> S.a
+    end
+end
+
+module Integer (S : Data.S) = struct
+  module Expr = Expr (S)
+
+  class ['visitor] t (value : int) =
+    object
+      constraint 'visitor = < integer : int -> S.a ; .. >
+      inherit ['visitor] Expr.t
+      method accept : 'visitor -> S.a = fun vis -> vis#integer value
+    end
+end
+
+module Add (S : Data.S) = struct
+  module Expr = Expr (S)
+
+  class ['visitor] t (lhs : 'visitor Expr.t) (rhs : 'visitor Expr.t) =
+    object
+      constraint
+      'visitor = < add : 'visitor. 'visitor Expr.t -> 'visitor Expr.t -> S.a
+                 ; .. >
+
+      inherit ['visitor] Expr.t
+      method accept : 'visitor -> S.a = fun vis -> vis#add lhs rhs
+    end
+end
+
+module Main (S : Data.S) = struct
+  module Expr = Expr (S)
+  module Integer = Integer (S)
+  module Add = Add (S)
+
+  let lhs = new Integer.t 1
+  let rhs = new Integer.t 2
+  let expr = new Add.t lhs rhs
+end
+
 (* 
-type 'a visitor = < >
-type 'a expr = < accept : 'a visitor >  *)
+let lhs :
+    < add : 'visitor 'a. 'visitor expr -> 'visitor expr -> 'a ; .. > integer =
+  new integer 1
 
-type ('self, 'a) visitor = < .. > as 'self
-type ('self, 'a) integer_visitor = < integer : int -> 'a ; .. > as 'self
-type ('self, 'a) add_visitor = < add : 'a -> 'a -> 'a ; .. > as 'self
+let rhs :
+    < add : 'visitor 'a. 'visitor expr -> 'visitor expr -> 'a ; .. > integer =
+  new integer 2
 
-class virtual expr =
-  object
-    method virtual accept : 'self 'a. ('self, 'a) visitor -> 'a
-  end
 
-let main (accept : ('self, 'a) integer_visitor) = accept#integer 0
+class my_vis = object 
+  method integer : int -> int = fun x -> x
+  method add : int -> int -> int = (+)
+end 
+let expr = new add lhs rhs *)
 
-class integer (value : int) =
-  object
-    method accept : 'self 'a. (< integer : int -> 'a ; .. > as 'self) -> 'a =
-      fun vis -> vis#integer value
-  end
+(* let result = expr#accept (new my_vis) *)
