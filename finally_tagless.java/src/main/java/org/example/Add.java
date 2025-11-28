5abe2dfd2c65731f7cc06e76ce05e856790dc387
猗露 <69190413+illusory0x0@users.noreply.github.com>
2025-11-28 23:18:29 +0800

    remove java example, javascript is enough

--- finally_tagless.java/src/main/java/org/example/Add.java
+++
@@ -1,7 +0,0 @@
-package org.example;
-
-import org.jetbrains.annotations.NotNull;
-
-public interface Add<Self> extends Expr<Self> {
-    @NotNull Self add(@NotNull Self lhs, @NotNull Self rhs);
-}
--- finally_tagless.java/src/main/java/org/example/Integer.java
+++
@@ -1,7 +0,0 @@
-package org.example;
-
-import org.jetbrains.annotations.NotNull;
-
-public interface Integer<Self> extends Expr<Self> {
-    @NotNull Self integer(int value);
-}
